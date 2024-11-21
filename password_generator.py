df0d0cc4740e22edc3f4bb26b0313b4e83d9e6c7
Hafsa. <178737731+HM-04@users.noreply.github.com>
2024-11-21 14:47:39 +0000

    Add while loop to add password

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,37 @@
 import string
 import random
 
-def password_generator(min_length, special characters="True", numbers="True"):
-    numbers = string.digits
+def password_generator(min_length, special_characters="True", numbers="True"):
+    digits = string.digits
     letters = string.ascii_letters
-    special_characters = string.punctuation
+    special = string.punctuation
 
+    characters = letters
     
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+    
+    password = " "
+    correct_requirements = False
+    number_present = False
+    special_present = False
+
+    while not correct_requirements or len(password) < min_length:
+        new_char = random.choice(characters)
+        password += new_char
+
+        if new_char in digits:
+            number_present = True
+        elif new_char in special_characters:
+            special_present = True
+
+        
+        correct_requirements = True
+        if numbers:
+            correct_requirements = number_present
+        if special_characters:
+            correct_requirements += correct_requirements + special_present
+        
+    return password
